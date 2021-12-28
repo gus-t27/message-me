87cbc38f6710e7160aaa70fcfe656ffefcfd094b
Gus Threlfall <gus.threlfall@gmail.com>
2021-12-28 13:40:49 +0000

    Include jquery and enable dropdown from semantic ui to work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,19 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("jquery");
+require("semantic-ui-sass");
 
-require("semantic-ui-sass")
-
-
+$(document).on("turbolinks:load", function () {
+  $(".ui.dropdown").dropdown();
+});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-
