04b19746fb13445a4d0bcfbc1b22a95aa6b317ec
Gus Threlfall <gus.threlfall@gmail.com>
2021-12-29 17:16:39 +0000

    Seeds file added with user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Big G", password: "password")
+User.create(username: "Samwise", password: "password")
+User.create(username: "Frodo", password: "password")
+User.create(username: "Boromir", password: "password")
+User.create(username: "Aragon", password: "password")
