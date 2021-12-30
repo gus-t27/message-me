a9228557c00480b9fe331a9e19031c38793f3a16
Gus Threlfall <gus.threlfall@gmail.com>
2021-12-30 11:31:55 +0000

    Javascript for closing flash messages added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,11 @@ require("semantic-ui-sass");
 
 $(document).on("turbolinks:load", function () {
   $(".ui.dropdown").dropdown();
+  $(".message .close").on("click", function () {
+    $(this).closest(".message").transition("fade");
+  });
 });
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
